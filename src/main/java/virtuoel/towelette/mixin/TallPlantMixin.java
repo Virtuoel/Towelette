86249667557470622184ff14243899ac60ed5657
Virtuoel <Virtuoel@users.noreply.github.com>
2019-01-28 19:01:30 +0300

    Fix tripwire hooks and tall plants voiding fluids

--- src/main/java/virtuoel/towelette/mixin/TallPlantMixin.java
+++ src/main/java/virtuoel/towelette/mixin/TallPlantMixin.java
@@ -6,12 +6,16 @@ import org.spongepowered.asm.mixin.Overwrite;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.TallPlantBlock;
+import net.minecraft.block.enums.DoubleBlockHalf;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IWorld;
 import net.minecraft.world.World;
 import virtuoel.towelette.hooks.FluidloggableHooks;
+import virtuoel.towelette.util.FluidUtils;
 
 @Mixin(TallPlantBlock.class)
 public abstract class TallPlantMixin extends BlockMixin
@@ -30,5 +34,12 @@ public abstract class TallPlantMixin extends BlockMixin
 	{
 		super.onPlaced(world_1, blockPos_1, blockState_1, livingEntity_1, itemStack_1);
 		FluidloggableHooks.hookTallBlockOnPlaced(world_1, blockPos_1, blockState_1, livingEntity_1, itemStack_1);
+	}
+	
+	@Overwrite
+	public void method_10021(IWorld iWorld_1, BlockPos blockPos_1, int int_1)
+	{
+		iWorld_1.setBlockState(blockPos_1, FluidUtils.getStateWithFluid(getDefaultState().with(Properties.DOUBLE_BLOCK_HALF, DoubleBlockHalf.LOWER), iWorld_1.getFluidState(blockPos_1)), int_1);
+		iWorld_1.setBlockState(blockPos_1.up(), FluidUtils.getStateWithFluid(getDefaultState().with(Properties.DOUBLE_BLOCK_HALF, DoubleBlockHalf.UPPER), iWorld_1.getFluidState(blockPos_1.up())), int_1);
 	}
 }
---
+++ src/main/java/virtuoel/towelette/mixin/TripwireHookBlockMixin.java
@@ -0,0 +1,21 @@
+package virtuoel.towelette.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.TripwireHookBlock;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import virtuoel.towelette.util.FluidUtils;
+
+@Mixin(TripwireHookBlock.class)
+public class TripwireHookBlockMixin
+{
+	@Redirect(method = "method_10776", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;setBlockState(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;I)Z"))
+	public boolean method_10776SetBlockStateProxy(World obj, BlockPos blockPos_1, BlockState blockState_1, int int_1)
+	{
+		return obj.setBlockState(blockPos_1, FluidUtils.getStateWithFluid(blockState_1, obj.getFluidState(blockPos_1)), int_1);
+	}
+}
