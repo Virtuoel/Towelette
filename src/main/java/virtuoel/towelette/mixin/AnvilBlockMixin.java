8abdac99cc36863aea470214e222be0634d2e154
Virtuoel <Virtuoel@users.noreply.github.com>
2019-02-24 17:53:07 +0300

    Fix damaging anvils and flower pot interactions removing fluids

---
+++ src/main/java/virtuoel/towelette/mixin/AnvilBlockMixin.java
@@ -0,0 +1,21 @@
+package virtuoel.towelette.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import net.minecraft.block.AnvilBlock;
+import net.minecraft.block.BlockState;
+import virtuoel.towelette.api.FluidProperty;
+import virtuoel.towelette.util.FluidUtils;
+
+@Mixin(AnvilBlock.class)
+public class AnvilBlockMixin
+{
+	@Inject(at = @At("RETURN"), method = "getLandingState", cancellable = true)
+	private static void onGetLandingState(BlockState blockState_1, CallbackInfoReturnable<BlockState> info)
+	{
+		info.setReturnValue(FluidUtils.getStateWithFluid(info.getReturnValue(), FluidProperty.FLUID.getFluidState(blockState_1)));
+	}
+}
