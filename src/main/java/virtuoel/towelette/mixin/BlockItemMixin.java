06f06634447ff603a8aed22e9ca9525df15855bd
Virtuoel <Virtuoel@users.noreply.github.com>
2019-03-29 17:28:08 +0300

    Maybe fix mixin issue in dev?

---
+++ src/main/java/virtuoel/towelette/mixin/BlockItemMixin.java
@@ -0,0 +1,21 @@
+package virtuoel.towelette.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.item.block.BlockItem;
+import virtuoel.towelette.util.FluidUtils;
+
+@Mixin(BlockItem.class)
+public abstract class BlockItemMixin
+{
+	@Redirect(method = "getBlockState", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/Block;getPlacementState(Lnet/minecraft/item/ItemPlacementContext;)Lnet/minecraft/block/BlockState;"))
+	public BlockState getBlockStateGetPlacementStateProxy(Block obj, ItemPlacementContext context)
+	{
+		return FluidUtils.getStateWithFluid(obj.getPlacementState(context), context);
+	}
+}
--- src/main/java/virtuoel/towelette/mixin/WallStandingBlockItemMixin.java
+++ src/main/java/virtuoel/towelette/mixin/WallStandingBlockItemMixin.java
@@ -7,15 +7,11 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.item.ItemPlacementContext;
-import net.minecraft.item.block.BlockItem;
 import net.minecraft.item.block.WallStandingBlockItem;
 import virtuoel.towelette.util.FluidUtils;
 
-@Mixin({
-	BlockItem.class,
-	WallStandingBlockItem.class,
-})
-public abstract class FluidloggableBlockPlacementMixin
+@Mixin(WallStandingBlockItem.class)
+public abstract class WallStandingBlockItemMixin
 {
 	@Redirect(method = "getBlockState", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/Block;getPlacementState(Lnet/minecraft/item/ItemPlacementContext;)Lnet/minecraft/block/BlockState;"))
 	public BlockState getBlockStateGetPlacementStateProxy(Block obj, ItemPlacementContext context)
