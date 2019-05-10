d683bb0f5671fb3316a17b9c3028b4fbe5e70747
Virtuoel <Virtuoel@users.noreply.github.com>
2019-05-10 15:43:08 +0300

    Properly working Bucket fixes

--- src/main/java/virtuoel/towelette/mixin/BucketItemMixin.java
+++ src/main/java/virtuoel/towelette/mixin/BucketItemMixin.java
@@ -7,6 +7,7 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.FluidFillable;
 import net.minecraft.block.Material;
@@ -25,16 +26,48 @@ public class BucketItemMixin
 {
 	@Shadow Fluid fluid;
 	
+	@Redirect(method = "use", at = @At(value = "FIELD", ordinal = 2, target = "Lnet/minecraft/item/BucketItem;fluid:Lnet/minecraft/fluid/Fluid;"))
+	public Fluid onUseFluidProxy(BucketItem this$0, World world_1, PlayerEntity playerEntity_1, Hand hand_1)
+	{
+		return Fluids.EMPTY;
+	}
+	
 	@Redirect(method = "use", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/BlockPos;offset(Lnet/minecraft/util/math/Direction;)Lnet/minecraft/util/math/BlockPos;"))
 	public BlockPos onUseOffsetProxy(BlockPos obj, Direction side, World world_1, PlayerEntity playerEntity_1, Hand hand_1)
 	{
 		if(world_1.getFluidState(obj).getFluid() != fluid)
 		{
 			final BlockState state = world_1.getBlockState(obj);
-			final Material material = state.getMaterial();
-			return !material.isSolid() || material.isReplaceable() || (state.getBlock() instanceof FluidFillable && ((FluidFillable) state.getBlock()).canFillWithFluid(world_1, obj, state, fluid)) ? obj : obj.offset(side);
+			final Block block = state.getBlock();
+			return block instanceof FluidFillable && ((FluidFillable) block).canFillWithFluid(world_1, obj, state, fluid) ? obj : obj.offset(side);
 		}
 		return obj.offset(side);
+	}
+	
+	@Redirect(method = "placeFluid", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/Material;isSolid()Z"))
+	public boolean onPlaceFluidIsSolidProxy(Material obj, @Nullable PlayerEntity playerEntity_1, World world_1, BlockPos blockPos_1, @Nullable BlockHitResult blockHitResult_1)
+	{
+		final boolean solid = obj.isSolid();
+		final BlockState state = world_1.getBlockState(blockPos_1);
+		final Block block = state.getBlock();
+		if(block instanceof FluidFillable)
+		{
+			return ((FluidFillable) block).canFillWithFluid(world_1, blockPos_1, state, fluid) ? solid : true;
+		}
+		return solid;
+	}
+	
+	@Redirect(method = "placeFluid", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/Material;isReplaceable()Z"))
+	public boolean onPlaceFluidIsReplaceableProxy(Material obj, @Nullable PlayerEntity playerEntity_1, World world_1, BlockPos blockPos_1, @Nullable BlockHitResult blockHitResult_1)
+	{
+		final boolean replaceable = obj.isReplaceable();
+		final BlockState state = world_1.getBlockState(blockPos_1);
+		final Block block = state.getBlock();
+		if(block instanceof FluidFillable)
+		{
+			return ((FluidFillable) block).canFillWithFluid(world_1, blockPos_1, state, fluid) || (world_1.getFluidState(blockPos_1).isEmpty() && replaceable);
+		}
+		return replaceable;
 	}
 	
 	@Redirect(method = "placeFluid", at = @At(value = "FIELD", ordinal = 3, target = "Lnet/minecraft/item/BucketItem;fluid:Lnet/minecraft/fluid/Fluid;"))
