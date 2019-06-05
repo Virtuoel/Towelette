061397af5a3e9d7432a167a8be3b73bb7bf295cb
Virtuoel <Virtuoel@users.noreply.github.com>
2019-06-05 21:11:08 +0300

    Remove rendering-related utility methods

--- src/main/java/virtuoel/towelette/util/FluidUtils.java
+++ src/main/java/virtuoel/towelette/util/FluidUtils.java
@@ -1,29 +1,18 @@
 package virtuoel.towelette.util;
 
-import java.io.IOException;
-import java.util.Optional;
-
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 import net.minecraft.block.BlockState;
 import net.minecraft.block.enums.SlabType;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.texture.MissingSprite;
-import net.minecraft.client.texture.Sprite;
-import net.minecraft.fluid.BaseFluid;
 import net.minecraft.fluid.Fluid;
 import net.minecraft.fluid.FluidState;
 import net.minecraft.fluid.Fluids;
 import net.minecraft.item.ItemUsageContext;
-import net.minecraft.resource.Resource;
 import net.minecraft.state.property.Properties;
-import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.registry.Registry;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.IWorld;
-import virtuoel.towelette.Towelette;
 import virtuoel.towelette.api.FluidProperty;
 
 public class FluidUtils
@@ -110,75 +99,5 @@ public class FluidUtils
 	private static void scheduleFluidTickImpl(Fluid fluid, IWorld world, BlockPos pos)
 	{
 		world.getFluidTickScheduler().schedule(pos, fluid, fluid.getTickRate(world));
-	}
-	
-	public static Identifier getIdForStillFluid(Fluid fluid)
-	{
-		if(!fluid.getDefaultState().isStill() && fluid instanceof BaseFluid)
-		{
-			fluid = ((BaseFluid) fluid).getStill();
-		}
-		return Registry.FLUID.getId(fluid);
-	}
-	
-	public static class Client
-	{
-		public static final Sprite MISSING_SPRITE = MinecraftClient.getInstance().getSpriteAtlas().getSprite(MissingSprite.getMissingSpriteId());
-		
-		public static Identifier withBlockPath(Identifier id)
-		{
-			return new Identifier(id.getNamespace(), "block/" + id.getPath());
-		}
-		
-		public static Identifier toTexturePath(Identifier id)
-		{
-			return new Identifier(id.getNamespace(), "textures/" + id.getPath() + ".png");
-		}
-		
-		public static Optional<Identifier> getSpriteIdForTexture(Identifier id)
-		{
-			Identifier path = toTexturePath(id);
-			try(Resource res = MinecraftClient.getInstance().getResourceManager().getResource(path))
-			{
-				return Optional.of(id);
-			}
-			catch(IOException e)
-			{
-				Towelette.LOGGER.debug("Could not load sprite {} from {}", id, path);
-				return Optional.empty();
-			}
-		}
-		
-		public static Optional<Identifier> getSpriteIdForFluid(Fluid fluid, String suffix)
-		{
-			Identifier id = withBlockPath(new Identifier(getIdForStillFluid(fluid).toString() + suffix));
-			
-			return getSpriteIdForTexture(id);
-		}
-		
-		public static Optional<Identifier> getSpriteIdForFluid(Fluid fluid, boolean still)
-		{
-			return getSpriteIdForFluid(fluid, still ? "_still" : "_flow");
-		}
-		
-		public static Sprite getSpriteForFluid(Fluid fluid, boolean still)
-		{
-			return getSprite(getSpriteIdForFluid(fluid, still));
-		}
-		
-		public static Optional<Identifier> getOverlaySpriteIdForFluid(Fluid fluid)
-		{
-			return getSpriteIdForFluid(fluid, "_overlay");
-		}
-		
-		public static Sprite getOverlaySpriteForFluid(Fluid fluid)
-		{
-			return getSprite(getOverlaySpriteIdForFluid(fluid));
-		}
-		
-		public static Sprite getSprite(Optional<Identifier> id)
-		{
-			return id.map(MinecraftClient.getInstance().getSpriteAtlas()::getSprite).orElse(MISSING_SPRITE);
-		}
 	}
 }
