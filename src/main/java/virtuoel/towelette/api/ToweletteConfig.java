f41b3c67c3d9bb326313e3547169bd7581aecda6
Virtuoel <Virtuoel@users.noreply.github.com>
2019-07-09 14:29:26 +0300

    Maybe safer classloading?

--- src/main/java/virtuoel/towelette/api/ToweletteConfig.java
+++ src/main/java/virtuoel/towelette/api/ToweletteConfig.java
@@ -1,15 +1,26 @@
 package virtuoel.towelette.api;
 
+import java.util.function.Supplier;
+
 import com.google.gson.JsonObject;
 
-import virtuoel.towelette.util.ConfigHandler;
-import virtuoel.towelette.util.JsonConfigHandler;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.util.Lazy;
 
 public class ToweletteConfig
 {
 	public static final String NAMESPACE = "towelette";
 	
-	public static final ConfigHandler<JsonObject> HANDLER = new JsonConfigHandler(NAMESPACE, NAMESPACE + "/config.json", ToweletteConfig::createDefaultConfig);
+	public static final Supplier<JsonObject> HANDLER =
+		!FabricLoader.getInstance().isModLoaded(NAMESPACE) ?
+		new Lazy<JsonObject>(JsonObject::new)::get :
+		((Supplier<Supplier<Supplier<JsonObject>>>)() ->
+		(() -> new virtuoel.towelette.util.JsonConfigHandler(
+			NAMESPACE,
+			NAMESPACE + "/config.json",
+			ToweletteConfig::createDefaultConfig
+		))).get().get();
+	
 	public static final JsonObject DATA = HANDLER.get();
 	
 	private static JsonObject createDefaultConfig()
