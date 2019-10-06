f1ee3405e0e3fbf3429ce215af196478bb40d52e
Virtuoel <Virtuoel@users.noreply.github.com>
2019-10-06 03:33:53 +0300

    Clean up easily avoidable Redirects

--- src/main/java/virtuoel/towelette/mixin/BambooBlockMixin.java
+++ src/main/java/virtuoel/towelette/mixin/BambooBlockMixin.java
@@ -2,19 +2,17 @@ package virtuoel.towelette.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 import net.minecraft.block.BambooBlock;
 import net.minecraft.fluid.FluidState;
 import net.minecraft.fluid.Fluids;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
 
 @Mixin(BambooBlock.class)
 public class BambooBlockMixin
 {
-	@Redirect(method = "getPlacementState(Lnet/minecraft/item/ItemPlacementContext;)Lnet/minecraft/block/BlockState;", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;getFluidState(Lnet/minecraft/util/math/BlockPos;)Lnet/minecraft/fluid/FluidState;"))
-	public FluidState getPlacementStateGetFluidStateProxy(World obj, BlockPos blockPos_1)
+	@ModifyVariable(method = "getPlacementState", at = @At(value = "LOAD", ordinal = 0), allow = 1, require = 1)
+	private FluidState getPlacementStateFluidStateProxy(FluidState state)
 	{
 		return Fluids.EMPTY.getDefaultState();
 	}
--- src/main/java/virtuoel/towelette/mixin/FallingBlockEntityMixin.java
+++ src/main/java/virtuoel/towelette/mixin/FallingBlockEntityMixin.java
@@ -1,24 +1,40 @@
 package virtuoel.towelette.mixin;
 
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
 import net.minecraft.block.BlockState;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.FallingBlockEntity;
+import net.minecraft.fluid.Fluids;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import virtuoel.towelette.util.FluidUtils;
 
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
-
 @Mixin(FallingBlockEntity.class)
-public class FallingBlockEntityMixin
+public abstract class FallingBlockEntityMixin extends Entity
 {
 	@Shadow BlockState block;
 	
-	@Redirect(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;setBlockState(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;I)Z"))
-	public boolean tickSetBlockStateProxy(World obj, BlockPos blockPos_1, BlockState blockState_1, int int_1)
+	private FallingBlockEntityMixin(EntityType<?> entityType, World world)
 	{
-		return obj.setBlockState(blockPos_1, this.block = FluidUtils.getStateWithFluid(blockState_1, obj, blockPos_1), int_1);
+		super(entityType, world);
+	}
+	
+	@Inject(at = @At("RETURN"), method = "<init>(Lnet/minecraft/world/World;DDDLnet/minecraft/block/BlockState;)V")
+	private void onConstruct(World world, double x, double y, double z, BlockState state, CallbackInfo info)
+	{
+		this.block = FluidUtils.getStateWithFluid(state, Fluids.EMPTY.getDefaultState());
+	}
+	
+	@ModifyArg(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;setBlockState(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;I)Z"))
+	private BlockState tickSetBlockStateProxy(BlockPos pos, BlockState state, int flags)
+	{
+		return this.block = FluidUtils.getStateWithFluid(state, this.world, pos);
 	}
 }
