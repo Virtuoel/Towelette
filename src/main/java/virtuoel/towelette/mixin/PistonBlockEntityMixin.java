aa46ddf7dfe3ff1770c73e7e2fefcd9eba87c0bc
Virtuoel <Virtuoel@users.noreply.github.com>
2019-10-12 20:09:04 +0300

    Fix crash with Carpet mod

--- src/main/java/virtuoel/towelette/mixin/PistonBlockEntityMixin.java
+++ src/main/java/virtuoel/towelette/mixin/PistonBlockEntityMixin.java
@@ -6,6 +6,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
@@ -14,6 +15,8 @@ import com.google.gson.JsonElement;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.block.entity.PistonBlockEntity;
 import net.minecraft.fluid.Fluids;
 import net.minecraft.util.math.BlockPos;
@@ -24,10 +27,15 @@ import virtuoel.towelette.api.ToweletteConfig;
 import virtuoel.towelette.util.FluidUtils;
 
 @Mixin(PistonBlockEntity.class)
-public abstract class PistonBlockEntityMixin
+public abstract class PistonBlockEntityMixin extends BlockEntity
 {
 	@Shadow BlockState pushedBlock;
 	@Shadow boolean source;
+	
+	private PistonBlockEntityMixin(BlockEntityType<?> noop)
+	{
+		super(noop);
+	}
 	
 	@Inject(at = @At("RETURN"), method = "<init>(Lnet/minecraft/block/BlockState;Lnet/minecraft/util/math/Direction;ZZ)V")
 	private void onConstruct(BlockState state, Direction facing, boolean extending, boolean source, CallbackInfo info)
@@ -48,23 +56,23 @@ public abstract class PistonBlockEntityMixin
 		return FluidUtils.getStateWithFluid(Block.getRenderingState(blockState, world, blockPos), Fluids.EMPTY.getDefaultState());
 	}
 	
-	@Redirect(method = "finish", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;setBlockState(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;I)Z"))
-	private boolean finishSetBlockStateProxy(World obj, BlockPos pos, BlockState state, int flags)
+	@ModifyArg(method = "finish", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;setBlockState(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;I)Z"))
+	private BlockState finishSetBlockStateProxy(BlockPos pos, BlockState state, int flags)
 	{
 		final boolean unpushableFluids = Optional.ofNullable(ToweletteConfig.DATA.get("unpushableFluids"))
 			.filter(JsonElement::isJsonPrimitive)
 			.map(JsonElement::getAsBoolean).orElse(true);
 		
-		return state == Blocks.AIR.getDefaultState() ? obj.clearBlockState(pos, false) : obj.setBlockState(pos, !unpushableFluids ? state : FluidUtils.getStateWithFluid(state, obj, pos), flags);
+		return state == Blocks.AIR.getDefaultState() ? this.world.getFluidState(pos).getBlockState() : !unpushableFluids ? state : FluidUtils.getStateWithFluid(state, this.world, pos);
 	}
 	
-	@Redirect(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;setBlockState(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;I)Z"))
-	private boolean tickSetBlockStateProxy(World obj, BlockPos pos, BlockState state, int flags)
+	@ModifyArg(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;setBlockState(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;I)Z"))
+	private BlockState tickSetBlockStateProxy(BlockPos pos, BlockState state, int flags)
 	{
 		final boolean unpushableFluids = Optional.ofNullable(ToweletteConfig.DATA.get("unpushableFluids"))
 			.filter(JsonElement::isJsonPrimitive)
 			.map(JsonElement::getAsBoolean).orElse(true);
 		
-		return unpushableFluids && state == Blocks.AIR.getDefaultState() ? obj.clearBlockState(pos, false) : obj.setBlockState(pos, !unpushableFluids && !source ? state : FluidUtils.getStateWithFluid(state, obj, pos), flags);
+		return (unpushableFluids && state == Blocks.AIR.getDefaultState()) ? this.world.getFluidState(pos).getBlockState() : (!unpushableFluids && !source) ? state : FluidUtils.getStateWithFluid(state, this.world, pos);
 	}
 }
