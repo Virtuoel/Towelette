38504d874ec6beea3b3c317b4d9987053f679324
Virtuoel <Virtuoel@users.noreply.github.com>
2019-10-19 18:10:17 +0300

    Schedule a fluid tick when a block is placed in a fluid

--- src/main/java/virtuoel/towelette/mixin/BlockStateMixin.java
+++ src/main/java/virtuoel/towelette/mixin/BlockStateMixin.java
@@ -4,6 +4,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import net.minecraft.block.BlockState;
@@ -12,6 +13,7 @@ import net.minecraft.fluid.Fluids;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.IWorld;
+import net.minecraft.world.World;
 import virtuoel.towelette.util.FluidUtils;
 
 @Mixin(BlockState.class)
@@ -33,9 +35,15 @@ public abstract class BlockStateMixin
 		}
 	}
 	
-	@Inject(at = @At("HEAD"), method = "getStateForNeighborUpdate")
+	@Inject(at = @At("RETURN"), method = "getStateForNeighborUpdate")
 	private void onGetStateForNeighborUpdate(Direction direction, BlockState blockState, IWorld world, BlockPos pos, BlockPos otherPos, CallbackInfoReturnable<BlockState> info)
 	{
 		FluidUtils.scheduleFluidTick((BlockState) (Object) this, world, pos);
+	}
+	
+	@Inject(at = @At("RETURN"), method = "onBlockAdded")
+	public void onOnBlockAdded(World world, BlockPos blockPos, BlockState blockState, boolean flag, CallbackInfo info)
+	{
+		FluidUtils.scheduleFluidTick((BlockState) (Object) this, world, blockPos);
 	}
 }
