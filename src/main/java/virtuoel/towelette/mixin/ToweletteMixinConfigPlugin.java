3a966ec12e8cac668023c8cf4a5d11855c58d3c1
Virtuoel <Virtuoel@users.noreply.github.com>
2019-11-29 20:41:15 +0300

    Fix mixin config plugin

--- src/main/java/virtuoel/towelette/mixin/ToweletteMixinConfigPlugin.java
+++ src/main/java/virtuoel/towelette/mixin/ToweletteMixinConfigPlugin.java
@@ -7,7 +7,7 @@ import java.util.Optional;
 import java.util.Set;
 
 import org.apache.commons.lang3.tuple.Pair;
-import org.spongepowered.asm.lib.tree.ClassNode;
+import org.objectweb.asm.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
 import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
 
