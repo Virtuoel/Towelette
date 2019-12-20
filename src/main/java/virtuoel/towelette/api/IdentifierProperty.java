de695af5fc1605a9819c440c96a6af3926b2f93c
Virtuoel <Virtuoel@users.noreply.github.com>
2019-12-21 01:45:57 +0300

    Deprecate moved properties

--- src/main/java/virtuoel/towelette/api/IdentifierProperty.java
+++ src/main/java/virtuoel/towelette/api/IdentifierProperty.java
@@ -1,51 +1,11 @@
 package virtuoel.towelette.api;
 
-import java.util.Collection;
-import java.util.LinkedHashSet;
-import java.util.Optional;
-
-import net.minecraft.state.property.AbstractProperty;
-import net.minecraft.util.Identifier;
-import virtuoel.statement.api.MutableProperty;
-
-public class IdentifierProperty extends AbstractProperty<Identifier> implements MutableProperty<Identifier>
+@Deprecated
+public class IdentifierProperty extends virtuoel.statement.api.property.IdentifierProperty
 {
-	private final Collection<Identifier> values = new LinkedHashSet<>();
-	
+	@Deprecated
 	public IdentifierProperty(String name)
 	{
-		super(name, Identifier.class);
-	}
-	
-	@Override
-	public Collection<Identifier> getValues()
-	{
-		return values;
-	}
-	
-	@Override
-	public Optional<Identifier> parse(final String valueName)
-	{
-		final int underscoreIndex = valueName.lastIndexOf('_');
-		if(underscoreIndex != -1)
-		{
-			try
-			{
-				final int namespaceLength = Integer.parseInt(valueName.substring(underscoreIndex + 1));
-				return Optional.of(new Identifier(valueName.substring(0, namespaceLength), valueName.substring(namespaceLength + 1, underscoreIndex)));
-			}
-			catch(NumberFormatException e)
-			{
-				
-			}
-		}
-		
-		return Optional.empty();
-	}
-	
-	@Override
-	public String name(Identifier value)
-	{
-		return value.getNamespace() + "_" + value.getPath() + "_" + value.getNamespace().length();
+		super(name);
 	}
 }
--- src/main/java/virtuoel/towelette/api/RegistryEntryProperty.java
+++ src/main/java/virtuoel/towelette/api/RegistryEntryProperty.java
@@ -1,41 +1,13 @@
 package virtuoel.towelette.api;
 
-import java.util.Optional;
-
-import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.DefaultedRegistry;
 import net.minecraft.util.registry.Registry;
 
-public class RegistryEntryProperty<T> extends IdentifierProperty
+@Deprecated
+public class RegistryEntryProperty<T> extends virtuoel.statement.api.property.RegistryEntryProperty<T>
 {
-	private final Registry<T> registry;
-	private final Identifier defaultId;
-	
+	@Deprecated
 	public RegistryEntryProperty(String name, Registry<T> registry)
 	{
-		super(name);
-		
-		this.registry = registry;
-		
-		if(this.registry instanceof DefaultedRegistry)
-		{
-			this.defaultId = ((DefaultedRegistry<T>) this.registry).getDefaultId();
-			add(this.defaultId);
-		}
-		else
-		{
-			this.defaultId = null;
-		}
-	}
-	
-	public Registry<T> getRegistry()
-	{
-		return registry;
-	}
-	
-	@Override
-	public Optional<Identifier> parse(final String valueName)
-	{
-		return Optional.of(super.parse(valueName).flatMap(registry::getOrEmpty).map(registry::getId).orElse(defaultId));
+		super(name, registry);
 	}
 }
