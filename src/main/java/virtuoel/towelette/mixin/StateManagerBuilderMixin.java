fb8cdd3c787ed043784cd5dd8e6c7452579472db
Virtuoel <Virtuoel@users.noreply.github.com>
2020-02-25 19:03:21 +0300

    Add properties after the block adds its own ones

---
+++ src/main/java/virtuoel/towelette/mixin/StateManagerBuilderMixin.java
@@ -0,0 +1,60 @@
+package virtuoel.towelette.mixin;
+
+import java.util.Map;
+import java.util.Optional;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonPrimitive;
+
+import net.minecraft.state.AbstractState;
+import net.minecraft.state.State;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.Property;
+import virtuoel.towelette.api.FluidProperties;
+import virtuoel.towelette.api.Fluidloggable;
+import virtuoel.towelette.api.ToweletteConfig;
+
+@Mixin(StateManager.Builder.class)
+public abstract class StateManagerBuilderMixin<O, S extends State<S>>
+{
+	@Shadow O owner;
+	@Shadow Map<String, Property<?>> namedProperties;
+	
+	@Shadow
+	abstract StateManager.Builder<O, S> add(Property<?>... properties);
+	
+	@Inject(at = @At("HEAD"), method = "build(Lnet/minecraft/state/StateManager$Factory;)Lnet/minecraft/state/StateManager;")
+	private <A extends AbstractState<O, S>> void onBuild(StateManager.Factory<O, S, A> factory, CallbackInfoReturnable<StateManager<O, S>> info)
+	{
+		if (owner instanceof Fluidloggable)
+		{
+			if (!namedProperties.containsKey(FluidProperties.FLUID.getName()))
+			{
+				add(FluidProperties.FLUID);
+			}
+			
+			if (Optional.ofNullable(ToweletteConfig.DATA.get("flowingFluidlogging"))
+				.filter(JsonElement::isJsonPrimitive).map(JsonElement::getAsJsonPrimitive)
+				.filter(JsonPrimitive::isBoolean).map(JsonPrimitive::getAsBoolean)
+				.orElse(false)
+			)
+			{
+				if (!namedProperties.containsKey(FluidProperties.LEVEL_1_8.getName()))
+				{
+					add(FluidProperties.LEVEL_1_8);
+				}
+				
+				if (!namedProperties.containsKey(FluidProperties.FALLING.getName()))
+				{
+					add(FluidProperties.FALLING);
+				}
+			}
+		}
+	}
+}
