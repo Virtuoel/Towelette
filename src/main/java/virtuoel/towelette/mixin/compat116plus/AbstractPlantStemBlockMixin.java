5972126b67ee645cc359423c6648c2e3a52543a4
Virtuoel <Virtuoel@users.noreply.github.com>
2020-03-14 03:03:36 +0300

    Don't duplicate fluid blocks on plant growth

--- src/main/java/virtuoel/towelette/mixin/compat116plus/AbstractPlantStemBlockMixin.java
+++ src/main/java/virtuoel/towelette/mixin/compat116plus/AbstractPlantStemBlockMixin.java
@@ -3,11 +3,13 @@ package virtuoel.towelette.mixin.compat116plus;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import net.minecraft.block.AbstractPlantPartBlock;
 import net.minecraft.block.AbstractPlantStemBlock;
 import net.minecraft.block.BlockState;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.IWorld;
@@ -28,5 +30,11 @@ public abstract class AbstractPlantStemBlockMixin extends AbstractPlantPartBlock
 		{
 			info.setReturnValue(FluidUtils.getStateWithFluid(info.getReturnValue(), world, pos));
 		}
+	}
+	
+	@Redirect(method = "grow", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/world/ServerWorld;setBlockState(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;)Z"))
+	private boolean growSetBlockStateProxy(ServerWorld obj, BlockPos blockPos, BlockState blockState)
+	{
+		return obj.setBlockState(blockPos, FluidUtils.getStateWithFluid(blockState, obj, blockPos));
 	}
 }
