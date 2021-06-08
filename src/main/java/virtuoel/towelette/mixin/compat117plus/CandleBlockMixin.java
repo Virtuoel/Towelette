f2b172c08eeb57c599fd36f8822186fed3d704b2
Virtuoel <Virtuoel@users.noreply.github.com>
2021-06-08 06:20:03 +0300

    Fix filling of candles and candle cakes

--- src/main/java/virtuoel/towelette/mixin/compat117plus/CandleBlockMixin.java
+++ src/main/java/virtuoel/towelette/mixin/compat117plus/CandleBlockMixin.java
@@ -5,10 +5,14 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import net.minecraft.block.AbstractCandleBlock;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.CandleBlock;
+import net.minecraft.fluid.FluidState;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.state.property.Properties;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.WorldAccess;
 import virtuoel.towelette.util.FluidUtils;
 import virtuoel.towelette.util.TagCompatibility;
 import virtuoel.towelette.util.ToweletteBlockStateExtensions;
@@ -23,6 +27,25 @@ public class CandleBlockMixin
 		if (((ToweletteFluidStateExtensions) (Object) context.getWorld().getFluidState(context.getBlockPos())).towelette_isIn(TagCompatibility.FluidTags.WATER))
 		{
 			info.setReturnValue(((ToweletteBlockStateExtensions) info.getReturnValue()).towelette_with(Properties.LIT, false).towelette_cast());
+		}
+	}
+	
+	@Inject(at = @At("RETURN"), method = "tryFillWithFluid(Lnet/minecraft/world/WorldAccess;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;Lnet/minecraft/fluid/FluidState;)Z", cancellable = true)
+	private void onTryFillWithFluid(WorldAccess world, BlockPos blockPos, BlockState blockState, FluidState fluidState, CallbackInfoReturnable<Boolean> info)
+	{
+		final ToweletteBlockStateExtensions state = (ToweletteBlockStateExtensions) blockState;
+		if (info.getReturnValue())
+		{
+			world.setBlockState(blockPos, FluidUtils.getStateWithFluid(state.towelette_with(Properties.LIT, false).towelette_cast(), fluidState), 3);
+		}
+		else if (FluidUtils.tryFillWithFluid(world, blockPos, state.towelette_with(Properties.LIT, false).towelette_cast(), fluidState))
+		{
+			if (state.<Boolean>towelette_get(Properties.LIT))
+			{
+				AbstractCandleBlock.extinguish(null, blockState, world, blockPos);
+			}
+			
+			info.setReturnValue(true);
 		}
 	}
 	
--- src/main/java/virtuoel/towelette/mixin/compat117plus/CandleCakeBlockMixin.java
+++ src/main/java/virtuoel/towelette/mixin/compat117plus/CandleCakeBlockMixin.java
@@ -6,21 +6,42 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import net.minecraft.block.AbstractCandleBlock;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.CandleCakeBlock;
+import net.minecraft.block.FluidFillable;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.WorldAccess;
 import virtuoel.towelette.util.FluidUtils;
 import virtuoel.towelette.util.TagCompatibility;
+import virtuoel.towelette.util.ToweletteBlockStateExtensions;
 
 @Mixin(CandleCakeBlock.class)
-public abstract class CandleCakeBlockMixin
+public abstract class CandleCakeBlockMixin implements FluidFillable
 {
 	@ModifyArg(method = "onUse", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/CakeBlock;tryEat(Lnet/minecraft/world/WorldAccess;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;Lnet/minecraft/entity/player/PlayerEntity;)Lnet/minecraft/util/ActionResult;"))
 	private BlockState onUseSetBlockStateProxy(WorldAccess obj, BlockPos pos, BlockState state, PlayerEntity player)
 	{
 		return FluidUtils.getStateWithFluid(state, obj, pos);
+	}
+	
+	@Override
+	public boolean tryFillWithFluid(WorldAccess world, BlockPos pos, BlockState state, FluidState fluidState)
+	{
+		if (FluidUtils.tryFillWithFluid(world, pos, state, fluidState))
+		{
+			if (((ToweletteBlockStateExtensions) state).<Boolean>towelette_get(Properties.LIT))
+			{
+				AbstractCandleBlock.extinguish((PlayerEntity) null, ((ToweletteBlockStateExtensions) world.getBlockState(pos)).towelette_with(Properties.LIT, false).towelette_cast(), world, pos);
+			}
+			
+			return true;
+		}
+		
+		return false;
 	}
 	
 	@Inject(at = @At("HEAD"), method = "canBeLit", cancellable = true)
