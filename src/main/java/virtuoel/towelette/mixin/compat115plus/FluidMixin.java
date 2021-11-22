5bfbca2de7a22bef1a6ee798591a60673a762d5d
Virtuoel <Virtuoel@users.noreply.github.com>
2021-11-22 19:56:05 +0300

    Remove unused code

--- src/main/java/virtuoel/towelette/mixin/compat115plus/FluidMixin.java
+++ src/main/java/virtuoel/towelette/mixin/compat115plus/FluidMixin.java
@@ -4,7 +4,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
 import net.minecraft.fluid.Fluid;
-import net.minecraft.tag.FluidTags;
 import net.minecraft.world.WorldAccess;
 import net.minecraft.world.WorldView;
 import virtuoel.towelette.util.ToweletteFluidExtensions;
@@ -17,7 +16,7 @@ public abstract class FluidMixin implements ToweletteFluidExtensions
 	
 	@Override
 	public int towelette_getTickRate(WorldAccess world)
-	{FluidTags.LAVA.getClass();
+	{
 		return getTickRate(world);
 	}
 }
