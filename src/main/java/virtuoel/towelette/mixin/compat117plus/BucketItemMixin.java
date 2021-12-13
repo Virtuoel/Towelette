499785e8d44f9647edbb3c9b11dc5e45dd6eeb74
Virtuoel <Virtuoel@users.noreply.github.com>
2021-12-13 17:58:45 +0300

    Fix mixin crash
    Fixes #37

--- src/main/java/virtuoel/towelette/mixin/compat117plus/BucketItemMixin.java
+++ src/main/java/virtuoel/towelette/mixin/compat117plus/BucketItemMixin.java
@@ -9,7 +9,6 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.At.Shift;
-import org.spongepowered.asm.mixin.injection.Coerce;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -28,6 +27,7 @@ import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 
 @Mixin(BucketItem.class)
@@ -43,7 +43,7 @@ public class BucketItemMixin extends Item
 	@Unique Optional<SoundEvent> sound = Optional.empty();
 	
 	@Inject(method = "use", locals = LocalCapture.CAPTURE_FAILHARD, at = @At(value = "INVOKE", shift = Shift.BEFORE, target = "Lnet/minecraft/block/FluidDrainable;tryDrainFluid(Lnet/minecraft/world/WorldAccess;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;)Lnet/minecraft/item/ItemStack;"))
-	private void onUse(World world, PlayerEntity user, Hand hand, CallbackInfoReturnable<TypedActionResult<ItemStack>> info, ItemStack itemStack, BlockHitResult blockHitResult, BlockPos blockPos, BlockPos blockPos2, BlockState blockState, FluidDrainable fluidDrainable, @Coerce Object noop)
+	private void onUse(World world, PlayerEntity user, Hand hand, CallbackInfoReturnable<TypedActionResult<ItemStack>> info, ItemStack itemStack, BlockHitResult blockHitResult, BlockPos blockPos, Direction dir, BlockPos blockPos2, BlockState blockState, FluidDrainable fluidDrainable)
 	{
 		final FluidState looking = world.getFluidState(blockPos);
 		sound = looking.getFluid().getBucketFillSound();
