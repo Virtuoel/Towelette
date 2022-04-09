c87a91f4feec5f50e10d618941d69389813487fd
Virtuoel <Virtuoel@users.noreply.github.com>
2022-04-09 20:47:57 +0300

    Don't use deprecated mod path method

--- src/main/java/virtuoel/towelette/util/ModContainerUtils.java
+++ src/main/java/virtuoel/towelette/util/ModContainerUtils.java
@@ -4,41 +4,60 @@ import java.io.IOException;
 import java.nio.file.FileSystem;
 import java.nio.file.Path;
 
+import it.unimi.dsi.fastutil.objects.Object2BooleanAVLTreeMap;
+import it.unimi.dsi.fastutil.objects.Object2BooleanMap;
 import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.ModContainer;
 
 public class ModContainerUtils
 {
+	private static final Object2BooleanMap<String> USER_ADDED_IDS_CACHE = new Object2BooleanAVLTreeMap<>();
+	
 	public static boolean isModUserAdded(final String modId)
 	{
-		try
+		synchronized (USER_ADDED_IDS_CACHE)
 		{
-			final ModContainer c = FabricLoader.getInstance().getModContainer(modId).get();
-			
-			if (c == null)
+			try
 			{
-				return false;
+				final ModContainer c = FabricLoader.getInstance().getModContainer(modId).get();
+				
+				if (c == null)
+				{
+					return false;
+				}
+				
+				if (USER_ADDED_IDS_CACHE.containsKey(modId))
+				{
+					return USER_ADDED_IDS_CACHE.getBoolean(modId);
+				}
+				
+				final Path gameDir = FabricLoader.getInstance().getGameDir().toRealPath();
+				final FileSystem gameFileSystem = gameDir.getFileSystem();
+				final boolean isDevEnv = FabricLoader.getInstance().isDevelopmentEnvironment();
+				
+				boolean isModDevEnv, inModsFolder;
+				Path containerPath;
+				FileSystem containerFileSystem;
+				for (final Path rootPath : c.getRootPaths())
+				{
+					containerPath = rootPath.toRealPath();
+					containerFileSystem = containerPath.getFileSystem();
+					isModDevEnv = isDevEnv && gameFileSystem == containerFileSystem;
+					inModsFolder = containerFileSystem.toString().startsWith(gameDir.toString());
+					
+					if (isModDevEnv || inModsFolder)
+					{
+						USER_ADDED_IDS_CACHE.put(modId, true);
+						return true;
+					}
+				}
+			}
+			catch (IOException e)
+			{
+				
 			}
 			
-			final Path gameDir = FabricLoader.getInstance().getGameDir().toRealPath();
-			final Path containerPath = c.getRootPath().toRealPath();
-			final FileSystem gameFileSystem = gameDir.getFileSystem();
-			final FileSystem containerFileSystem = containerPath.getFileSystem();
-			
-			boolean userAdded = false;
-			
-			final boolean isDevEnv = FabricLoader.getInstance().isDevelopmentEnvironment();
-			final boolean isModDevEnv = isDevEnv && gameFileSystem == containerFileSystem;
-			final boolean inModsFolder = containerFileSystem.toString().startsWith(gameDir.toString());
-			userAdded = isModDevEnv || inModsFolder;
-			
-			return userAdded;
+			return false;
 		}
-		catch (IOException e)
-		{
-			
-		}
-		
-		return false;
 	}
 }
