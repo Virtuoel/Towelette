a9348ed555c67ace480372c50a0b0f02144724ec
Virtuoel <Virtuoel@users.noreply.github.com>
2022-04-11 01:11:00 +0300

    Let mangrove roots be fluidloggable

--- src/main/java/virtuoel/towelette/mixin/fluidloggable/compat119plus/FluidloggableBlockMarkerMixin.java
+++ src/main/java/virtuoel/towelette/mixin/fluidloggable/compat119plus/FluidloggableBlockMarkerMixin.java
@@ -3,6 +3,7 @@ package virtuoel.towelette.mixin.fluidloggable.compat119plus;
 import org.spongepowered.asm.mixin.Mixin;
 
 import net.minecraft.block.LeavesBlock;
+import net.minecraft.block.MangroveRootsBlock;
 import net.minecraft.block.SculkShriekerBlock;
 import net.minecraft.block.SculkVeinBlock;
 import virtuoel.towelette.util.AutomaticFluidloggableMarker;
@@ -10,6 +11,7 @@ import virtuoel.towelette.util.AutomaticWaterloggableMarker;
 
 @Mixin({
 	LeavesBlock.class,
+	MangroveRootsBlock.class,
 	SculkVeinBlock.class,
 	SculkShriekerBlock.class
 })
